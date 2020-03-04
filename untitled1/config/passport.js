1d82f0b984923455c93cc7cbaf657d3a09a81024
hayarto1 <artavazd.h@hedgehogshub.com>
2020-03-04 18:18:07 +0400

    env

--- untitled1/config/passport.js
+++ untitled1/config/passport.js
@@ -1,11 +1,11 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
-const config = require('./db');
+
 module.exports = function (passport) {
     let opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-    opts.secretOrKey = config.secret;
+    opts.secretOrKey = process.env.DATABASE_SECRET;
     passport.use(new JwtStrategy(opts, ( jwt_payload, done ) => {
         User.getUserById(jwt_payload._id, (err, user) => {
             if(err) {
--- untitled1/models/user.js
+++ untitled1/models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const config = require('../config/db');
 
 const UserSchema = mongoose.Schema({
     firstName: {
